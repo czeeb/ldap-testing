d77f0483584cdbec4ac6381a7a89bb495b821c7e
Chris Zeeb <czeeb@zam.com>
2013-03-08 17:55:37 -0500

    Mostly working.

--- Vagrantfile
+++ Vagrantfile
@@ -4,6 +4,7 @@
 Vagrant::Config.run do |config|
   config.vm.box = "precise64"
     config.vm.box_url = "http://files.vagrantup.com/precise64.box"
+    config.vm.forward_port 80,8080
 
     config.vm.customize(['storagectl', :id, '--name', 'SATA Controller', '--hostiocache', 'on'])
     config.vm.customize(['setextradata', :id, 'VBoxInternal2/SharedFoldersEnableSymlinksCreate/v-root', '1'])
--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -12,3 +12,59 @@ package { "git":
   ensure => present,
 }
 
+# LDAP Server
+package { "slapd": 
+  ensure => present,
+}
+
+package { "ldap-utils":
+  ensure => present,
+}
+
+package { "phpldapadmin":
+  ensure => present,
+}
+
+# LDAP Client
+package { "nscd":
+  ensure => present,
+}
+
+package { 'libpam-ldap':
+  ensure => present,
+}
+
+file { 
+  '/etc/ldap.conf':
+    require => Package["libpam-ldap"],
+    ensure  => present,
+    source  => "/vagrant/puppet/files/ldap-client.conf";
+  '/etc/ldap.secret':
+    require => Package["libpam-ldap"],
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => 0600,
+    source => "/vagrant/puppet/files/ldap-client.secret";
+  '/etc/nsswitch.conf':
+    require => Package["libpam-ldap"],
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => 0644,
+    source => "/vagrant/puppet/files/nsswitch.conf";
+  '/etc/pam.d/common-password':
+    require => Package["libpam-ldap"],
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => 0644,
+    source => '/vagrant/puppet/files/common-password';
+  '/etc/pam.d/common-session':
+    require => Package["libpam-ldap"],
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => 0644,
+    source  => '/vagrant/puppet/files/common-session';
+}
